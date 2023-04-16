e222d3f2e3b2b7ea4f0d58802ca115663f900c3a
士子☀️ <84996057@qq.com>
2023-04-16 13:56:21 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,6 @@ import { Haptics, ImpactStyle } from '@capacitor/haptics'
 import { StatusBar, Style } from '@capacitor/status-bar'
 import { RouterView } from 'vue-router'
 import { Tabbar, TabbarItem, NavBar } from 'vant'
-import { ref } from 'vue'
-
-const active = ref('/')
 
 const onChange = async () => {
   await Haptics.impact({ style: ImpactStyle.Light })
@@ -26,7 +23,7 @@ init()
 
   <RouterView />
 
-  <Tabbar v-model="active" @change="onChange">
+  <Tabbar route @change="onChange">
     <TabbarItem replace to="/" icon="home-o">Home</TabbarItem>
     <TabbarItem replace to="/about" icon="search">Search</TabbarItem>
     <TabbarItem replace to="/about" icon="friends-o">Friends</TabbarItem>
